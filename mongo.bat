2fd461271027d4eda22655d8bdff350243159465
funnybobby <funnybobby2@gmail.com>
2020-07-28 23:39:28 +0200

    upgrade mongodb to 4.2

--- mongo.bat
+++ mongo.bat
@@ -1,2 +1,2 @@
 TITLE MongoDB
-"C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe" --dbpath "C:\Users\funnybobby\Desktop\Projets\cookingBookAngular\backend\dataBase"
+"C:\Program Files\MongoDB\Server\4.2\bin\mongod.exe" --dbpath "C:\Users\funnybobby\Desktop\Projets\cookingBookAngular\backend\dataBase"
